4b85b16fdc83ccb73f6a09e1c445c488f6b3d1f7
sabbirNew <sabbir.ahmed5936@gmail.com>
2021-11-02 11:33:15 +0600

    heroku error handle port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port =5000;
 
 app.use(cors());
 app.use(express.json());
@@ -56,6 +56,28 @@ client.connect((err) => {
     const result = await registerCollection.find({ email: email }).toArray();
     res.send(result);
   });
+  app.post("/addService", async (req, res) => {
+    const newService = req.body;
+    const result = await serviceCollection.insertOne(newService);
+    res.send(result);
+    console.log(result);
+  });
+
+// update status
+  app.put("/allregister/:id", async (req, res) => {
+    const id = req.params.id;
+    const updateStatus = req.body;
+    const find = { _id: ObjectId(id) };
+    const updateDoc = {
+      $set: {
+        status: updateStatus.status,
+      },
+    };
+    const result = await registerCollection.updateOne(find, updateDoc);
+    res.json(result);
+  });
+
+
   //   client.close();
 });
 
@@ -63,6 +85,6 @@ app.get("/", (req, res) => {
   res.send("Getting successfully");
 });
 
-app.listen(port, () => {
+app.listen( process.env.port || PORT, () => {
   console.log("listening on port", port);
 });
